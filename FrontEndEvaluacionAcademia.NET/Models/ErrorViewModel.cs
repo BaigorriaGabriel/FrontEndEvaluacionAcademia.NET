1b1f030462f01d7a1a65aca8fd4a081c58c4b731
BaigorriaGabriel <gabi.2912@hotmail.com>
2023-10-25 10:57:47 -0300

    Agregar archivos de proyecto.

---
+++ FrontEndEvaluacionAcademia.NET/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FrontEndEvaluacionAcademia.NET.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
